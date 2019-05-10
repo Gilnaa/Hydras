84efc96afbcc99b971ec7edef25db97f436f1898
Gilad Naaman <gilad@naaman.io>
2019-05-10 18:09:41 +0300

    Added PyPi metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+      long_description = f.read()
+
 setup(name='Hydras',
       version='1.0.0',
       description='A module for constructions of structured binary packets.',
       author='Gilad Naaman',
       author_email='gilad@naaman.io',
       tests_require=["pytest"],
-      packages=['hydras'])
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/Gilnaa/Hydras",
+      packages=['hydras'],
+      classifiers=[
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+      ])
