f777e50cc3fccdccd18a623a0b51e0eef5fb7449
Gilad Naaman <gilad@naaman.io>
2019-10-21 11:10:36 +0300

    v2.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
       long_description = f.read()
 
 setup(name='Hydras',
-      version='2.0.1',
+      version='2.0.2',
       description='A module for constructions of structured binary packets.',
       author='Gilad Naaman',
       author_email='gilad@naaman.io',
